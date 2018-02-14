7245bdeba68557f25239d4b2584d86aff9bdadce
Alex Kite <alexkite511@gmail.com>
2018-02-13 16:09:12 -0800

    vertical thrust working

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const canvasEl = document.getElementsByTagName("canvas")[0];
 canvasEl.height = window.innerHeight;
 canvasEl.width = window.innerWidth;
 
-console.log(canvasEl);
+
 new Game(
   canvasEl.width,
   canvasEl.height
