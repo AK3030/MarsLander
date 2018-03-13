6cdb2036a7fe0447d95f2a206ed9bd74a3568009
Alex Kite <alexkite511@gmail.com>
2018-03-13 15:35:42 -0700

    thruster tweak

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import Game from "./lib/game";
 const canvasEl = document.getElementsByTagName("canvas")[0];
 
 canvasEl.height = 700;
-canvasEl.width = 550;
+canvasEl.width = 1000;
 
 new Game(
   canvasEl.width,
